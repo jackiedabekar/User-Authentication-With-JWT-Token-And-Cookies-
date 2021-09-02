1682f594e7ab7467443036f6b24230affb788607
deepakdabekar <deepakdabekar@nimapinfotech.com>
2021-09-02 13:15:56 +0530

    Created User Login View

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import RegisterView
+from .views import RegisterView, LoginView
 
 app_name = 'user'
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='user-register')
+    path('register/', RegisterView.as_view(), name='user-register'),
+    path('login/', LoginView.as_view(), name='user-login')
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 from .serializers import UserSerializer
-
-
+from .models import User
 
 class RegisterView(APIView):
     def post(self, request):
@@ -10,3 +10,22 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            raise AuthenticationFailed('User Not Found')
+
+        # We cant compare password received in request with password
+        # of user save in DB which is in Hash format, so we are using inbuilt function
+        # to compare password.
+        if not user.check_password(password):
+            raise AuthenticationFailed('Password is incorrect')
+
+        return Response({
+            'message': 'success'
+        })
